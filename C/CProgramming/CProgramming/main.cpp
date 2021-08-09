1722469053afbe00bdc606f596bcb13408bb30a9
BckLily <52183009+BckLily@users.noreply.github.com>
2021-08-10 00:04:53 +0900

    팀원 문제 예시 코드
    if문...을 원했는데 if문만 사용하는 것이 없어서 대충 비슷한 문제들...

--- C/CProgramming/CProgramming/main.cpp
+++ C/CProgramming/CProgramming/main.cpp
@@ -11,10 +11,10 @@
 //
 //	/*
 //	10~20 사이 숫자 입력,
-//	1부터 입력 받은 숫자 사이 값 출력.
+//	1부터 입력 받은 숫자 사이 합 출력.
 //	*/
 //
-//	printf("\n10~20 사이 숫자 입력, 1부터 입력 받은 숫자 사이 값 출력.\n");
+//	printf("\n10~20 사이 숫자 입력, 1부터 입력 받은 숫자 사이 합 출력.\n");
 //
 //	int input;
 //	while (1) {
@@ -76,17 +76,17 @@
 //	정수를 반복하여 입력받아 누적 합계, 0입력시 while 종료.
 //	*/
 //	
-//	printf("\n정수를 반복하여 입력받아 누적 합계, 0입력시 while 종료.\n");
-//
-//	sum = 0;
-//
-//	while (1) {
-//		scanf(" %d", &input);
-//		if (input == 0) { break; }
-//		sum += input;
-//	} 
-//
-//	printf("%d\n", sum);
+	//printf("\n정수를 반복하여 입력받아 누적 합계, 0입력시 while 종료.\n");
+
+	//sum = 0;
+
+	//while (1) {
+	//	scanf(" %d", &input);
+	//	if (input == 0) { break; }
+	//	sum += input;
+	//} 
+
+	//printf("%d\n", sum);
 //
 //
 //	/*
@@ -113,35 +113,35 @@
 //
 //		i++;
 //	}
-//	/*
-//	while(temp < input){
-//	if(input % temp == 0){
-//	printf("소수 아님");
-//	break;
-//	}
-//	temp++;
-//	}
-//	if(input <= 1){
-//	printf("1 이상만 가능");
-//	}
-//	else if(temp == input){
-//	printf("소수");
-//	}
-//	*/
+	//
+	//while(temp < input){
+	//if(input % temp == 0){
+	//printf("소수 아님");
+	//break;
+	//}
+	//temp++;
+	//}
+	//if(input <= 1){
+	//printf("1 이상만 가능");
+	//}
+	//else if(temp == input){
+	//printf("소수");
+	//}
+	//
 //
 //
 //	/*
 //	입력한 수 반대로 출력.
 //	*/
 //
-//	printf("\n입력한 수 반대로 출력\n");
-//
-//	scanf(" %d", &input);
-//	
-//	while (input > 0) {
-//		printf("%d", (input % 10));
-//		input /= 10;
-//	}
+	//printf("\n입력한 수 반대로 출력\n");
+
+	//scanf(" %d", &input);
+	//
+	//while (input > 0) {
+	//	printf("%d", (input % 10));
+	//	input /= 10;
+	//}
 //
 //
 //	/*
@@ -166,23 +166,23 @@
 //	// for문
 //	////////////////////////////////
 //
-//	//printf("\n1 >>\n");
-//	//
-//	//for (int i = 1; i <= 9; i++) {
-//	//	if (i % 2) { printf("%d.Hello\n", i); }
-//	//}
+	//printf("\n1 >>\n");
+	//
+	//for (int i = 1; i <= 9; i++) {
+	//	if (i % 2) { printf("%d.Hello\n", i); }
+	//}
 //
 //
 //
 //	//printf("\n2 >>\n");
 //
-//	//// ASCII 관련...
-//	//for (char c = 'A'; c <= 'Z'; c++) {
-//	//	printf("%c,", c);
-//	//	if (c == 'M') { printf("\n"); }
-//	//}
-//
-//	//printf("\n");
+	//// ASCII 관련...
+	//for (char c = 'A'; c <= 'Z'; c++) {
+	//	printf("%c,", c);
+	//	if (c == 'M') { printf("\n"); }
+	//}
+
+	//printf("\n");
 //
 //
 //
@@ -202,118 +202,147 @@
 //	return 0;
 ////}
 
-/*
+//
+//int main(void) {
+//
+//	ConsoleWindowSize();	//	콘솔 창 크기 변경
+//
+//	FontColorChange(GREEN); // 글자 색 변경
+//	printf("4) 첫 날 1원을 예금하고, 다음날부터 전날의 2배씩 예금하는 방식으로 한 달(30일) 동안 저축한 금액은?\n");
+//	FontColorChange(WHITE); // 글자 색 변경
+//
+//	int save = 1; // 저축할 금액
+//	int sum = 0; // 저축한 금액
+//
+//	for (int i = 0; i < 30; i++, save *= 2) {
+//		sum += save;
+//	}
+//
+//	std::cout << sum + "원";
+//
+//	return 0;
+//}
+
+//
+//int main(void) {
+//	ConsoleWindowSize();
+//
+//	FontColorChange(GREEN);
+//	printf("5) 1~1000까지의 누적 합계를 구하라. 단 3의 배수는 제외하며, 3과 5의 공배수는 제외하지 않는다.\n");
+//	FontColorChange(WHITE);
+//
+//	int sum = 0;
+//	// 1~1000까지
+//	for (int i = 1; i <= 1000; i++) {
+//		// i가 3의 배수이고 5의 배수가 아닐 때 (공배수 제외)
+//		if (i % 3 == 0 && i % 5 != 0) {
+//			// 뒷 부분 건너뛰고 증감문으로 이동.
+//			continue;
+//		}
+//		sum += i;
+//	}
+//
+//	std::cout << sum;
+//
+//	return 0;
+//}
+
+//
+//int main(void) {
+//	ConsoleWindowSize();
+//
+//	FontColorChange(GREEN);
+//	std::cout << "6) 1부터 시작하여 홀수의 합을 구하면서, 그 합이 10000을 넘기 직전의 i의 수를 구하시오.\n";
+//	FontColorChange(WHITE);
+//
+//	
+//	int sum = 0; // 합계를 저장할 변수
+//	int i = 1; // 증가할 카운트 변수
+//
+//	// 반복을 하면서 매번 i를 1증가시킨다.
+//	for (; ; i++) {
+//		// i가 홀수일 경우 sum에 i값을 더한다.
+//		if (i % 2 == 1) { sum += i; }
+//		// sum이 10000을 넘는 경우
+//		if (sum > 10000) {
+//			// sum에서 현재 i값을 빼고
+//			sum -= i;
+//			// i에서 2를 빼서 이전 홀수 값으로 만든다.
+//			i -= 2;
+//			break;
+//		}
+//	}
+//	// 출력
+//	std::cout << "i: " + std::to_string(i) + " sum: " + std::to_string(sum);
+//
+//
+//	return 0;
+//}
+
+//
+//int main(void) {
+//	ConsoleWindowSize();
+//
+//	float start = clock();
+//
+//
+//	FontColorChange(GREEN);
+//	std::cout << "# 실행파일과 같이 구구단을 출력하시오. (다중 for문)\n";
+//	FontColorChange(WHITE);
+//
+//	for (int i = 2; i <= 9; i++) {
+//		// 좌측 정렬
+//		std::cout.setf(std::ios::left);
+//		// 출력 폭 지정
+//		std::cout.width(16);
+//		// 출력
+//		std::cout << "==== " <<i << "단 ====";
+//		// 빈 공간을 무엇으로 채울지 설정.
+//		std::cout.fill(' ');
+//	}
+//	std::cout << "\n";
+//	for (int j = 1; j <= 9; j++) {
+//
+//		for (int i = 2; i <= 9; i++) {
+//			std::cout.setf(std::ios::left);
+//			std::cout.width(16);
+//			std::cout << i << " X " << j << " = " << j * i;
+//		}
+//		std::cout << std::endl;
+//	}
+//
+//	float end = clock();
+//
+//	std::cout << "time: " << end - start << "ms";
+//
+//	return 0;
+//}
+
+
 int main(void) {
 
-	ConsoleWindowSize();	//	콘솔 창 크기 변경
+	int i = 0, total = 0;
 
-	FontColorChange(GREEN); // 글자 색 변경
-	printf("4) 첫 날 1원을 예금하고, 다음날부터 전날의 2배씩 예금하는 방식으로 한 달(30일) 동안 저축한 금액은?\n");
-	FontColorChange(WHITE); // 글자 색 변경
-
-	int save = 1; // 저축할 금액
-	int sum = 0; // 저축한 금액
-
-	for (int i = 0; i < 30; i++, save *= 2) {
-		sum += save;
-	}
-
-	std::cout << sum + "원";
-
-	return 0;
-}
-*/
-/*
-int main(void) {
-	ConsoleWindowSize();
-
-	FontColorChange(GREEN);
-	printf("5) 1~1000까지의 누적 합계를 구하라. 단 3의 배수는 제외하며, 3과 5의 공배수는 제외하지 않는다.\n");
-	FontColorChange(WHITE);
-
-	int sum = 0;
-	// 1~1000까지
 	for (int i = 1; i <= 1000; i++) {
-		// i가 3의 배수이고 5의 배수가 아닐 때 (공배수 제외)
-		if (i % 3 == 0 && i % 5 != 0) {
-			// 뒷 부분 건너뛰고 증감문으로 이동.
-			continue;
+		if (i % 3 == 0) {
+			if (i % 5 == 0) {
+				total += i;
+			}
 		}
-		sum += i;
+		else {
+			total += i;
+		}
 	}
 
-	std::cout << sum;
+	printf("%d\n", total);
 
 	return 0;
 }
-*/
-/*
-int main(void) {
-	ConsoleWindowSize();
-
-	FontColorChange(GREEN);
-	std::cout << "6) 1부터 시작하여 홀수의 합을 구하면서, 그 합이 10000을 넘기 직전의 i의 수를 구하시오.\n";
-	FontColorChange(WHITE);
-
-	
-	int sum = 0; // 합계를 저장할 변수
-	int i = 1; // 증가할 카운트 변수
-
-	// 반복을 하면서 매번 i를 1증가시킨다.
-	for (; ; i++) {
-		// i가 홀수일 경우 sum에 i값을 더한다.
-		if (i % 2 == 1) { sum += i; }
-		// sum이 10000을 넘는 경우
-		if (sum > 10000) {
-			// sum에서 현재 i값을 빼고
-			sum -= i;
-			// i에서 2를 빼서 이전 홀수 값으로 만든다.
-			i -= 2;
-			break;
-		}
-	}
-	// 출력
-	std::cout << "i: " + std::to_string(i) + " sum: " + std::to_string(sum);
 
 
-	return 0;
-}
-*/
-
-int main(void) {
-	ConsoleWindowSize();
-
-	float start = clock();
-
-
-	FontColorChange(GREEN);
-	std::cout << "# 실행파일과 같이 구구단을 출력하시오. (다중 for문)\n";
-	FontColorChange(WHITE);
-
-	for (int i = 2; i <= 9; i++) {
-		// 좌측 정렬
-		std::cout.setf(std::ios::left);
-		// 출력 폭 지정
-		std::cout.width(16);
-		// 출력
-		std::cout << "==== " + std::to_string(i) + "단 ====";
-		// 빈 공간을 무엇으로 채울지 설정.
-		std::cout.fill(' ');
-	}
-	std::cout << "\n";
-	for (int j = 1; j <= 9; j++) {
-
-		for (int i = 2; i <= 9; i++) {
-			std::cout.setf(std::ios::left);
-			std::cout.width(16);
-			std::cout << std::to_string(i) + " X " + std::to_string(j) + " = " + std::to_string(j * i);
-		}
-		std::cout << "\n";
-	}
-
-	float end = clock();
-
-	std::cout << "time: " << std::to_string(end - start) << "ms";
-
-	return 0;
-}
+//int main(void) {
+//
+//
+//
+//	return 0;
+//}
---
+++ C/CProgramming/Team_If/main.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include "ConsoleColorChange.h"
+
+//int main(void) {
+//
+//	FontColorChange(GREEN);
+//	std::cout << "초를 입력 받은 다음 [시, 분, 초]로 출력" << std::endl;
+//	FontColorChange(WHITE);
+//	int sec;
+//	int min;
+//	int hour;
+//
+//	// 초 입력.
+//	std::cin >> sec;
+//
+//	// 분에 초를 60으로 나눈 몫을 대입
+//	min = sec / 60;
+//	// 초를 60으로 나눈 나머지를 대입
+//	sec %= 60;
+//
+//	hour = min / 60;
+//	min %= 60;
+//
+//	std::cout << "[h:" << hour << ", m:" << min << ", s:" << sec << "]" << std::endl;
+//
+//	return 0;
+//}
+
+//int main(void) {
+//	FontColorChange(GREEN);
+//	printf("초를 입력 받은 다음 [시, 분, 초]로 출력\n");
+//	FontColorChange(WHITE);
+//
+//	int sec;
+//	int min;
+//	int hour;
+//
+//	scanf_s(" %d", &sec);
+//
+//	min = sec / 60;
+//	sec %= 60;
+//	hour = min / 60;
+//	min %= 60;
+//	printf("[h:%d, m:%d, s:%d]\n", hour, min, sec);
+//
+//
+//	return 0;
+//}
+
+
+//int main(void) {
+//	FontColorChange(GREEN);
+//	std::cout << "키보드로 수식을 입력하면 계산 결과를 출력하는 프로그램을 작성한다. 정수 사칙연산만 입력." << std::endl;
+//	FontColorChange(WHITE);
+//
+//
+//	int a;
+//	char calc;
+//	int b;
+//	int c;
+//
+//	std::cin >> a >> calc >> b;
+//
+	//switch (calc) {
+	//case '+':
+	//	c = a + b;
+	//	break;
+	//case '-':
+	//	c = a - b;
+	//	break;
+	//case '*':
+	//	c = a * b;
+	//	break;
+	//case '/':
+	//	c = a / b;
+	//	break;
+	//default:
+	//	break;
+	//}
+//
+//	std::cout << a << calc << b << "=" << c << std::endl;
+//
+//	return 0;
+//}
+
+//
+//int main(void) {
+//	FontColorChange(GREEN);
+//	printf("키보드로 수식을 입력하면 계산 결과를 출력하는 프로그램을 작성한다. 정수 사칙연산만 입력.\n");
+//	FontColorChange(WHITE);
+//
+//	int a;
+//	char calc;
+//	int b;
+//	int c;
+//
+//	scanf_s(" %d", &a);
+//	scanf_s(" %c", &calc);
+//	scanf_s(" %d", &b);
+//
+//	switch (calc) {
+//	case '+':
+//		c = a + b;
+//		break;
+//	case '-':
+//		c = a - b;
+//		break;
+//	case '*':
+//		c = a * b;
+//		break;
+//	case '/':
+//		c = a / b;
+//		break;
+//	default:
+//
+//		break;
+//	}
+//
+//	printf("%d%c%d=%d", a, calc, b, c);
+//
+//
+//	return 0;
+//}
+
+int main(void) {
+
+
+
+}
